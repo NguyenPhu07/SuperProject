d672b333ea0360c0643825d78e68c4c833fbbf43
NguyenPhu07 <truyduoitv0@gmail.com>
2022-08-26 19:48:08 +0700

    cơdsa

--- Khoa-BoMon/Subject.js
+++ Khoa-BoMon/Subject.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $("div.figure > .item").addClass("wow animate__fadeInUp")
+    $("div.first > .item").addClass("animate__animated animate__fadeInUp")
     wow = new WOW({
         boxClass:     'wow',      // default
         animateClass: 'animate__animated', // default
@@ -8,4 +9,33 @@ $(document).ready(function(){
         live:         true        // default
       })
       wow.init();
+      //scroll chuột
+    $(window).scroll(function()
+    {
+        if($(this).scroll)
+        {
+            $('nav.menu-nav').addClass('sticky');
+            $('nav.menu-nav').css({
+                "z-index":"9999",
+            })
+        }
+        
+        else
+        $('nav.menu-nav').removeClass('sticky');
+    })
+
+    $('#goTop').hide();
+    $(window).scroll(function()
+    {
+        if($(this).scrollTop() >= 200)
+        $('#goTop').show("slow");
+        else
+        $('#goTop').hide("slow");
+    })
+
+    $("#goTop").click(function(){
+        $("html, body").animate({
+            scrollTop: 0,}
+        ,1000)
+    });
 })
